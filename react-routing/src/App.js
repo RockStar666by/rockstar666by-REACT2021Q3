7bbe68312395e071e44b06802015cf13108faf5d
Artiom Savchuk <savchuk799@gmail.com>
2021-08-17 21:55:20 +0300

    add: react-routing

---
+++ react-routing/src/App.js
@@ -0,0 +1,13 @@
+import './index.css';
+import React from 'react';
+import Router from './components/Router/Router.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Router />
+    </div>
+  );
+}
+
+export default App;
